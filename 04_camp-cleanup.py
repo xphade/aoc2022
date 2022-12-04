0d58593e059be1b94204f6d1c9fdc295f16b6f74
xphade <18196286+xphade@users.noreply.github.com>
2022-12-04 13:18:37 +0100

    Improve day 4 solution

--- 04_camp-cleanup.py
+++ 04_camp-cleanup.py
@@ -8,22 +8,29 @@ from typing import List, Tuple
 
 from aoc_utils import get_input_path, print_elapsed_time
 
+Range = Tuple[int, int]
+Pair = List[Range]
 
-def preprocess_input(contents: List[str]) -> List[Tuple[int]]:
-    assignment_pairs = []
+
+def preprocess_input(contents: List[str]) -> List[Pair]:
+    assignment_pairs: List[Pair] = []
     for line in contents:
-        range_pair = [tuple(map(int, rng.split("-"))) for rng in line.split(",")]
+        range_pair: Pair = [tuple(map(int, rng.split("-"))) for rng in line.split(",")]
         assignment_pairs.append(range_pair)
     return assignment_pairs
 
 
-def count_fully_contained_ranges(pairs: List[Tuple[int]]) -> int:
-    # Sort pairs by length.
-    sorted_pairs = [sorted(pair, key=lambda x: x[1] - x[0]) for pair in pairs]
-    return sum(x[0] >= y[0] and x[1] <= y[1] for x, y in sorted_pairs)
+def count_fully_contained_ranges(pairs: List[Pair]) -> int:
+    def lhs_in_rhs(lhs: Range, rhs: Range) -> bool:
+        return lhs[0] >= rhs[0] and lhs[1] <= rhs[1]
+
+    def rhs_in_lhs(lhs: Range, rhs: Range) -> bool:
+        return rhs[0] >= lhs[0] and rhs[1] <= lhs[1]
+
+    return sum(lhs_in_rhs(x, y) or rhs_in_lhs(x, y) for x, y in pairs)
 
 
-def count_overlapping_ranges(pairs: List[Tuple[int]]) -> int:
+def count_overlapping_ranges(pairs: List[Pair]) -> int:
     # Sort pairs by first index (small before big).
     sorted_pairs = [sorted(pair, key=lambda x: x[0]) for pair in pairs]
     return sum((x[1] - y[0]) >= 0 for x, y in sorted_pairs)
